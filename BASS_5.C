6b39d9f3fb8943c0b74066bac571244766acacf1
lindosdev <71178627+lindosdev@users.noreply.github.com>
2023-01-03 11:50:52 -0500

    Update BASS_5.C

--- BASS_5.C
+++ BASS_5.C
@@ -3,7 +3,7 @@
 int main(void)
 {
 int choice;
-  printf ("Fretboard Notes & Frequencies.\n Copyright C 2010-2022  Gary J. Teixeira Jr.\n\n You can redistribute this program and/or modify\n it under the terms of the Creative Commons\n Attribution-NonCommercial 4.0 International License.\n\n This program is distributed in the hope that it will be useful,\n but WITHOUT ANY WARRANTY without even the implied warranty of\n MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\n You may contact the developer of this program at  ambientmine at protonmail.com\n");
+  printf ("Fretboard Notes & Frequencies.\n Copyright C 2010-2023  Gary J. Teixeira Jr.\n\n You can redistribute this program and/or modify\n it under the terms of the Creative Commons\n Attribution-NonCommercial 4.0 International License.\n\n This program is distributed in the hope that it will be useful,\n but WITHOUT ANY WARRANTY without even the implied warranty of\n MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\n You may contact the developer of this program at  ambientmine at protonmail.com\n");
   printf ("5-String Bass\n\n");
   printf ("1. -7 Frets 2. -6 Frets 3. -5 Frets 4. -4 Frets 5. -3 Frets 6. -2 Frets 7. -1 Frets 8. Standard Tuning\n");
   printf ("9. +1 Frets 10. +2 Frets 11. +3 Frets 12. +4 Frets 13. +5 Frets 14. +6 Frets 15. +7 Frets 16. +8 Frets\n");
