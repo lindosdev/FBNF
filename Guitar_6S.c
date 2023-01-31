8e2e0c7038dce3147249365d22a78f492a377fca
lindosdev <71178627+lindosdev@users.noreply.github.com>
2023-01-30 23:50:00 -0500

    Update Guitar_6S.c

--- Guitar_6S.c
+++ Guitar_6S.c
@@ -5,7 +5,8 @@ int main(void)
 int choice;
   printf ("Fretboard Notes & Frequencies.\n Copyright C 2010-2023  Gary J. Teixeira Jr.\n\n You can redistribute this program and/or modify\n it under the terms of the Creative Commons\n Attribution-NonCommercial 4.0 International License.\n\n This program is distributed in the hope that it will be useful,\n but WITHOUT ANY WARRANTY without even the implied warranty of\n MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\n You may contact the developer of this program at  ambientmine at protonmail.com\n");
   printf ("6-String Guitar \n\n");
-  printf ("1. -24 Frets  2. -23 Frets  \n");
+  printf ("1. -24 Frets  2. -23 Frets  3. -22 Frets  4. -21 Frets  5. -20 Frets  6. -19 Frets\n");
+  printf ("7. -18 Frets  8. -17 Frets  9. -16 Frets  10. -15 Frets  11. -14 Frets  11. -13 Frets  \n");
 
   printf("Enter choice ");
   scanf ("%d", &choice);
